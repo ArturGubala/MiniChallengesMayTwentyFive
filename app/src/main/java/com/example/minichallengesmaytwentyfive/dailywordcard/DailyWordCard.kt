bb3792386326c205a274056babd2099c212523ff
arturgie <gubala-artur@wp.pl>
2025-05-14 21:45:03 +0200

    Fix background gradient.

--- app/src/main/java/com/example/minichallengesmaytwentyfive/dailywordcard/DailyWordCard.kt
+++ app/src/main/java/com/example/minichallengesmaytwentyfive/dailywordcard/DailyWordCard.kt
@@ -1,7 +1,9 @@
 package com.example.minichallengesmaytwentyfive.dailywordcard
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -14,22 +16,28 @@ import androidx.compose.ui.tooling.preview.Preview
 @Composable
 fun DailyWordCard() {
     Scaffold(
-        modifier = Modifier
-            .background(
-                brush = Brush.verticalGradient(
-                    colors = listOf(
-                        Color(0xFF6B74F8),
-                        Color(0xFFFDE5F3),
-                        Color(0xFFFEF7EE)
+        contentWindowInsets = WindowInsets(0, 0, 0, 0)
+    ) { padding ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .background(
+                    brush = Brush.verticalGradient(
+                        colors = listOf(
+                            Color(0xFF6B74F8),
+                            Color(0xFFFDE5F3),
+                            Color(0xFFFEF7EE)
+                        )
                     )
                 )
-            )
-    ) { padding ->
-        Column(
-            modifier = Modifier
-                .padding(padding)
-                .fillMaxSize()
-        ) {  }
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(padding)
+            ) {  }
+        }
     }
 }
 
