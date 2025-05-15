eade61a351a7219c302a3dd022fc270f369cf6a4
arturgie <gubala-artur@wp.pl>
2025-05-15 21:46:08 +0200

    Add button and place for text in UI.

--- app/src/main/java/com/example/minichallengesmaytwentyfive/dailywordcard/DailyWordCard.kt
+++ app/src/main/java/com/example/minichallengesmaytwentyfive/dailywordcard/DailyWordCard.kt
@@ -1,17 +1,29 @@
 package com.example.minichallengesmaytwentyfive.dailywordcard
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.minichallengesmaytwentyfive.R
 
 @Composable
 fun DailyWordCard() {
@@ -35,8 +47,47 @@ fun DailyWordCard() {
             Column(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(padding)
-            ) {  }
+                    .padding(padding),
+                verticalArrangement = Arrangement.spacedBy(40.dp, Alignment.CenterVertically),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Box(
+                    modifier = Modifier
+                        .size(364.dp)
+                        .shadow(
+                            elevation = 20.dp,
+                            shape = RoundedCornerShape(12.dp),
+                            ambientColor = Color(0x13182C0D),
+                            spotColor = Color(0x13182C0D)
+                        )
+                        .background(
+                            color = Color.White,
+                            shape = RoundedCornerShape(20.dp)
+                        )
+                ) {
+
+                }
+                Button(
+                    onClick = {  },
+                    modifier = Modifier
+                        .size(80.dp)
+                        .shadow(
+                            elevation = 20.dp,
+                            shape = RoundedCornerShape(12.dp),
+                            ambientColor = Color(0x1413182C),
+                            spotColor = Color(0x1413182C)
+                        ),
+                    shape = CircleShape,
+                    colors = ButtonDefaults.buttonColors(
+                      containerColor = Color(0xFF6B74F8)
+                    )
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.ic_volume_max),
+                        contentDescription = null
+                    )
+                }
+            }
         }
     }
 }
