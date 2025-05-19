a2862c1e7fc4cfab36b4c886cd826add0cca3d31
arturgie <gubala-artur@wp.pl>
2025-05-19 22:34:06 +0200

    Add search bar to SearchableStudyList.

--- app/src/main/java/com/example/minichallengesmaytwentyfive/searchablestudylist/SearchableStudyList.kt
+++ app/src/main/java/com/example/minichallengesmaytwentyfive/searchablestudylist/SearchableStudyList.kt
@@ -1,7 +1,5 @@
 package com.example.minichallengesmaytwentyfive.searchablestudylist
 
-import android.speech.tts.TextToSpeech
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -10,27 +8,19 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.minichallengesmaytwentyfive.R
 import com.example.minichallengesmaytwentyfive.ui.theme.MiniChallengesMayTwentyFiveTheme
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -38,13 +28,35 @@ import com.example.minichallengesmaytwentyfive.ui.theme.MiniChallengesMayTwentyF
 fun SearchableStudyList() {
     Scaffold(
         topBar = {
-            CenterAlignedTopAppBar(
-                title = {
-                    Text(
-                        text = "Study Topics"
-                    )
-                }
-            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 4.dp, start = 20.dp, bottom = 12.dp, end = 20.dp)
+            ) {
+                Text(
+                    text = "Study Topics",
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textAlign = TextAlign.Center
+                )
+                SearchBar(
+                    inputField = {
+                        SearchBarDefaults.InputField(
+                            query = "",
+                            onQueryChange = { },
+                            onSearch = {
+                            },
+                            expanded = false,
+                            onExpandedChange = {  },
+                            placeholder = { Text("Search") }
+                        )
+                    },
+                    expanded = false,
+                    onExpandedChange = { },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                ) {}
+            }
         },
         contentWindowInsets = WindowInsets(0, 0, 0, 0)
     ) { padding ->
